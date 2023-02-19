55a32356e69685632c5304882148a0614f990620
Arshadali <112824636+arshadali-15@users.noreply.github.com>
2023-02-19 19:33:08 +0530

    Update add_task_screen.dart

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,42 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task_data.dart';
 
 class AddTaskScreen extends StatelessWidget {
-  const AddTaskScreen({Key? key}) : super(key: key);
+  late String newTask;
+  int i = 1;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
-      color: Color(0xff757575),
+      //color: Color(0xff757575),
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
         decoration: BoxDecoration(
-          color: Colors.white,
+            color: Colors.black,
             borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20))
-        ),
-
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text('Add Task',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.lightBlueAccent,
-            ),),
-            SizedBox(height: 20,),
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
             TextField(
               autofocus: true,
               textAlign: TextAlign.center,
-            ),SizedBox(height: 20,),
-            TextButton(onPressed:(){},
-                child : Text('Add',style: TextStyle(
-                  color: Colors.white,
+              onChanged: (newText) {
+                newTask = newText;
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: () {
+                Provider.of<TaskData>(context, listen: false).addTask(newTask);
+                Navigator.pop(context);
+                final snackBar = SnackBar(
+                  content: const Text('Click and hold to delete a task!'),
+                  action: SnackBarAction(
+                    label: 'Got it',
+                    onPressed: () {
+
+                    },
+                  ),
+                );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.black,
                 ),
-                ),
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.lightBlueAccent)),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.lightBlueAccent)),
             ),
           ],
         ),
