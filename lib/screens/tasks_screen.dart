fe2590c7b2b6c08fdefbc4c54fe6bc11f605573d
Arshadali <112824636+arshadali-15@users.noreply.github.com>
2023-02-19 19:34:17 +0530

    Update tasks_screen.dart

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todoey/screens/add_task_screen.dart';
-
 import '../widgets/tasks_list.dart';
-
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task_data.dart';
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({Key? key}) : super(key: key);
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +15,8 @@ class TasksScreen extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.lightBlueAccent,
           onPressed: () {
-              showModalBottomSheet(context: context, builder: (context)=>AddTaskScreen());
+            showModalBottomSheet(
+                context: context, builder: (context) => AddTaskScreen());
           },
           child: Icon(
             Icons.add,
@@ -39,7 +38,6 @@ class TasksScreen extends StatelessWidget {
                       size: 35,
                       color: Colors.lightBlueAccent,
                     ),
-                    backgroundColor: Colors.white,
                     radius: 28,
                   ),
                   SizedBox(
@@ -48,13 +46,13 @@ class TasksScreen extends StatelessWidget {
                   Text(
                     'ToDoey',
                     style: TextStyle(
-                        color: Colors.white,
+                        color: Colors.black,
                         fontSize: 50,
                         fontWeight: FontWeight.w700),
                   ),
                   Text(
-                    '12 Tasks',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+                    '${Provider.of<TaskData>(context).tasksCount} Tasks',
+                    style: TextStyle(color: Colors.black54, fontSize: 20),
                   ),
                 ],
               ),
@@ -63,15 +61,14 @@ class TasksScreen extends StatelessWidget {
               child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 30),
                 decoration: BoxDecoration(
-                    color: Colors.white,
+                    color: Colors.black,
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(20),
                         topRight: Radius.circular(20))),
-                child: TaskList(),
+                child: TasksList(),
               ),
             ),
           ],
         ));
   }
 }
-
